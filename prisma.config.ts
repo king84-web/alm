8d856e368325b655b3171c23146a14eac5c1b409
king84_web <gggggggggggghhhhhhhh581@gmail.com>
2026-02-16 22:16:07 +0200

    fix: remove prisma.config.ts to resolve module conflict

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig, env } from 'prisma/config'
-
-export default defineConfig({
-  schema: 'prisma/schema.prisma',
-  datasource: {
-    // Change the hardcoded URL back to this:
-    url: env('DATABASE_URL'), 
-  },
-})
