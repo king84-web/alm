53c7209dd5995762e6e63299dae38b2cae701331
king84_web <gggggggggggghhhhhhhh581@gmail.com>
2026-02-16 23:19:19 +0200

    fix: update PrismaService to work with Prisma 6 (remove adapter)

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,24 +1,8 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../generated/prisma-client/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  constructor() {
-    const pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
-    });
-
-    const adapter = new PrismaPg(pool);
-
-    // Pass adapter to PrismaClient
-    super({ adapter });
-  }
-
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
     await this.$connect();
   }
